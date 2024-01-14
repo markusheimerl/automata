7dff59a04a7818b1c9d5ee39a5dfe761912d73ba
Markus Heimerl <149831926+markusheimerl@users.noreply.github.com>
2024-01-14 09:33:26 +0000

    Remove LALR parser and related files

--- finite_automaton/deterministic_finite_automaton/deterministic_finite_automaton.c
+++
@@ -1,9 +0,0 @@
-#include "deterministic_finite_automaton.h"
-
-deterministic_finite_automaton DeterministicFiniteAutomaton(set states, set alphabet, delta_function delta, word start, set final_states)
-{
-    return NTuple(5, states, alphabet, delta, start, final_states);
-}
-/*
-deterministic_finite_automaton nfaDFA(nondeterministic_finite_automaton nfa) {return NULL;}
-*/
