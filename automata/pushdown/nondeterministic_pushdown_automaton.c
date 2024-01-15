d4f31003d6532e19af02d70610d8f322da33b90f
Markus Heimerl <149831926+markusheimerl@users.noreply.github.com>
2024-01-15 19:06:58 +0000

    Update devcontainer.json and add NondeterministicPushDownAutomaton constructor

--- automata/pushdown/nondeterministic_pushdown_automaton.c
+++ automata/pushdown/nondeterministic_pushdown_automaton.c
@@ -1,2 +1,5 @@
 #include "nondeterministic_pushdown_automaton.h"
 
+nondeterminisitc_pushdown_automaton NondeterministicPushDownAutomaton(set states, set input_alphabet, set stack_alphabet, function delta_function, word start_state, set final_states){
+    return NTuple(6, states, input_alphabet, stack_alphabet, delta_function, start_state, final_states);
+}
--- automata/pushdown/nondeterministic_pushdown_automaton.h
+++ automata/pushdown/nondeterministic_pushdown_automaton.h
@@ -2,7 +2,12 @@
 #define NONDETERMINISTIC_PUSHDOWN_AUTOMATON_H
 
 #include "n_tuple.h"
+#include "set.h"
+#include "function.h"
+#include "word.h"
 
 typedef n_tuple nondeterminisitc_pushdown_automaton;
+
+nondeterminisitc_pushdown_automaton NondeterministicPushDownAutomaton(set states, set input_alphabet, set stack_alphabet, function delta_function, word start_state, set final_states);
 
 #endif // NONDETERMINISTIC_PUSHDOWN_AUTOMATON_H
