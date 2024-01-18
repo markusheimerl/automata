da88ca7c2f7b836b55d9a1d56461cabd84bd1981
Markus Heimerl <149831926+markusheimerl@users.noreply.github.com>
2024-01-18 12:33:19 +0000

    Refactor npa_delta_function.c

--- automata/pushdown/npa_delta_function.c
+++ automata/pushdown/npa_delta_function.c
@@ -5,28 +5,3 @@ npa_delta_function NPADeltaFunction(void)
 {
     return Function();
 }
-
-npa_delta_function relationToNPADeltaFunction(relation delta_relation)
-{
-
-}
-
-npa_delta_function addToNPADeltaFunction(npa_delta_function f, word w, letter let, set to)
-{
-
-}
-
-set getNPADeltaFunctionValue(npa_delta_function f, word w, letter let)
-{
-
-}
-
-word getWordFromNPADeltaFunctionDomainElement(set from)
-{
-
-}
-
-letter getLetterFromNPADeltaFunctionDomainElement(set from)
-{
-
-}
