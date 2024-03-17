ffd17b4e0f5b0e5c218489e5761cb9f48e2bf434
Markus Heimerl <149831926+markusheimerl@users.noreply.github.com>
2024-03-17 11:55:51 +0000

    Removed pushdown automata

--- automata/pushdown/nondeterministic_pushdown_automaton.c
+++
@@ -1,6 +0,0 @@
-#include "nondeterministic_pushdown_automaton.h"
-
-nondeterminisitc_pushdown_automaton NondeterministicPushDownAutomaton(set states, set input_alphabet, set stack_alphabet, npa_delta_function delta, word start_state, set final_states)
-{
-    return NTuple(6, states, input_alphabet, stack_alphabet, delta, start_state, final_states);
-}
--- automata/pushdown/npa_delta_function.c
+++
@@ -1,7 +0,0 @@
-#include "npa_delta_function.h"
-#include "n_tuple.h"
-
-npa_delta_function NPADeltaFunction(void)
-{
-    return Function();
-}
